67536873928bb3b5c624e2214108087faf800713
Alymbekov <maxim.makarov.1997@mail.ru>
2019-01-31 20:27:35 +0600

    add a vote logic and finished the hard way

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.http import HttpResponse , HttpResponseRedirect
 from django.shortcuts import get_object_or_404,render
+from django.urls import reverse
+from .models import Choice, Question
 from .models import Question
 
 def index(request):
@@ -14,7 +17,18 @@ def detail(request,question_id):
     return render(request,'polls/detail.html',{'question':question})
 
 def results(request,question_id):
-    response = "You are looking at the reults of question %s."
-    return HttpResponse("YOu are voting on question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/results.html',{'question':question})
 def vote(request,question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':question,
+            'error_message': "You didn't select a choice .",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
